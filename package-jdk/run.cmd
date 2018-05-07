4db42b02054fed97e1b603d8dbd0c913e0c4e774
Shisei Hanai <ruimo.uno@gmail.com>
2018-05-07 16:43:44 +0900

    create package for windows/linux.

---
+++ package-jdk/run.cmd
@@ -0,0 +1,18 @@
+cd %~dp0
+
+if exist formbuilder (
+  echo rd /s /q app jdk run run.cmd > update.cmd
+  echo move formbuilder\app . >> update.cmd
+  echo move formbuilder\jdk . >> update.cmd
+  echo move formbuilder\run . >> update.cmd
+  echo move formbuilder\run.cmd . >> update.cmd
+  echo rd /s /q formbuilder
+  echo run.cmd
+
+  update.cmd
+) else (
+  del update.cmd
+)
+
+set JAVA_HOME=jdk
+app\formbuilder\bin\formbuilder.bat
